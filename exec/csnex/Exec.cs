a3418b3994ec061d3354d070dc63ba2fe0c3f603
Larry Frieson <larryf@mlinks.net>
2021-05-24 11:23:14 -0700

    Finish interface support in csnex

--- exec/csnex/Exec.cs
+++ exec/csnex/Exec.cs
@@ -230,6 +230,24 @@ namespace csnex
         void PUSHPMG()
         {
             throw new NotImplementedException(string.Format("{0} not implemented.", MethodBase.GetCurrentMethod().Name));
+            //ip++;
+            //int mod = Bytecode.Get_VInt(module.Bytecode.code, ref ip);
+            //int name = Bytecode.Get_VInt(module.Bytecode.code, ref ip);
+
+            //if (!modules.ContainsKey(module.Bytecode.strtable[mod])) {
+            //    throw new NeonException(string.Format("module not found: {0}\n", module.Bytecode.strtable[mod]));
+            //}
+            //Module m = modules[module.Bytecode.strtable[mod]];
+
+            //for (int v = 0; v < m.Bytecode.variables.Count; v++) {
+            //    if (string.Compare(m.Bytecode.strtable[m.Bytecode.variables[v].name], module.Bytecode.strtable[name]) == 0) {
+            //        int addr = m.Bytecode.variables[v].index;
+            //        Debug.Assert(addr < m.Bytecode.globals.Count);
+            //        stack.Push(Cell.CreateAddressCell(m.Bytecode.globals[addr]));
+            //        return;
+            //    }
+            //}
+            //throw new NeonException(string.Format("variable not found: {0}\n", module.Bytecode.strtable[name]));
         }
 
         void PUSHPL()
@@ -255,7 +273,9 @@ namespace csnex
 
         void PUSHI()
         {
-            throw new NotImplementedException(string.Format("{0} not implemented.", MethodBase.GetCurrentMethod().Name));
+            ip++;
+            int x = Bytecode.Get_VInt(module.Bytecode.code, ref ip);
+            stack.Push(Cell.CreateNumberCell(new Number(x)));
         }
 
         void PUSHNIL()
@@ -981,7 +1001,20 @@ namespace csnex
 
         void CALLV()
         {
-            throw new NotImplementedException(string.Format("{0} not implemented.", MethodBase.GetCurrentMethod().Name));
+            ip++;
+            int val = Bytecode.Get_VInt(module.Bytecode.code, ref ip);
+            if (callstack.Count >= param_recursion_limit) {
+                Raise("StackOverflowException", "");
+                return;
+            }
+
+            Cell pi = stack.Pop();
+            Cell instance = pi.Array[0].Address;
+            int interface_index = Number.number_to_int32(pi.Array[1].Number);
+            Module m = (Module)instance.Array[0].Array[0].Other;
+            Bytecode.ClassInfo classinfo = (Bytecode.ClassInfo)instance.Array[0].Array[1].Other;
+
+            Invoke(m, classinfo.interfaces[interface_index].methods[val]);
         }
 #endregion
 #region JUMP Opcodes
